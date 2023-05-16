0e4a8a57568133dfe7a924ccb8135f600a1306fb
Philip Napoli <dubomb1011@gmail.com>
2023-05-16 19:21:03 -0400

    Implemented deck functionality

---
+++ Blackjack/src/deckutility.cpp
@@ -0,0 +1,34 @@
+#include "deckutility.h"
+
+Deck::Deck()
+	: current_card(0)
+{
+	generator = std::default_random_engine(device());
+}
+
+void Deck::shuffle()
+{
+	current_deck = std::array<int, 52>(Deck::default_deck);
+	std::shuffle(current_deck.begin(), current_deck.end(), generator);
+
+	current_card = 0;
+}
+
+int Deck::draw_card()
+{
+	int card = current_deck[current_card];
+
+	current_card++;
+
+	return card;
+}
+
+int Deck::get_cards_drawn() const
+{
+	return current_card;
+}
+
+const std::array<int, 52> Deck::default_deck = 
+		{ 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+		6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10,
+		11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13 };
