27d5b9a552e273ad0da0457beb27f77ececc62ef
Philip Napoli <dubomb1011@gmail.com>
2023-05-18 12:58:55 -0400

    Documented deck utility files

--- Blackjack/src/deckutility.cpp
+++ Blackjack/src/deckutility.cpp
@@ -1,28 +1,41 @@
+// 
+// file: deckutility.cpp
+// 
+// Implementation of the Deck class
+// Provides functions to simulate drawing cards from a deck
+// 
+// @author Philip Napoli
+// 
+
 #include "deckutility.h"
 
-#include <iostream>
+#include <array>
 
+/// Initializes the random number generator
+/// Also sets the current deck to the start deck and current card to the start
 Deck::Deck()
 	: current_card(0)
 {
 	generator = std::default_random_engine(device());
+	current_deck = std::array<int, 52>(Deck::start_deck);
 }
 
+/// Randomizes the order of the cards in the deck
+/// Resets the current card to reveal back to the start
 void Deck::shuffle()
 {
-	current_deck = std::array<int, 52>(Deck::default_deck);
 	std::shuffle(current_deck.begin(), current_deck.end(), generator);
 
 	current_card = 0;
 }
 
+/// Draws a card from the deck and returns the numerical value
+/// If the last card in the deck has been drawn, the deck is reshuffled
 int Deck::draw_card()
 {
 	int card = current_deck[current_card];
 
 	current_card++;
-
-	std::cout << current_card << "\n";
 
 	if (current_card >= 52)
 	{
@@ -32,12 +45,18 @@ int Deck::draw_card()
 	return card;
 }
 
+/// Returns the number of cards that have been drawn from the current deck
 int Deck::get_cards_drawn() const
 {
 	return current_card;
 }
 
-const std::array<int, 52> Deck::default_deck = 
+// 1: Ace
+// 2-10: Respective values
+// 11: Jack
+// 12: Queen
+// 13: King
+const std::array<int, 52> Deck::start_deck = 
 		{ 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
 		6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10,
 		11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13 };
--- Blackjack/src/deckutility.h
+++ Blackjack/src/deckutility.h
@@ -1,25 +1,45 @@
+// 
+// file: deckutility.h
+// 
+// Header file for the Deck class
+// Provides functions to simulate drawing cards from a deck
+// 
+// @author Philip Napoli
+// 
+
 #pragma once
 
-#include <array>
 #include <random>
 
+/// Class representing a deck of 52 cards
+/// Provides functions for drawing and shuffling the deck
 class Deck
 {
 public:
+	/// Constructor for initializing the deck of cards for usage
 	Deck();
 
+	/// Randomizes the order of the cards in the deck
+	/// Resets the position in the deck to begin drawing from the top
 	void shuffle();
 
+	/// Removes a card from the deck and provides the removed card
+	/// Reshuffles the deck if the last card has been drawn
+	/// 
+	/// @return A numerical representation of the card that was removed
 	int draw_card();
 
+	/// Provides the number of cards that have been drawn from the deck
+	/// 
+	/// @return The number of cards drawn
 	int get_cards_drawn() const;
 
 private:
-	static const std::array<int, 52> default_deck;
+	static const std::array<int, 52> start_deck; ///< Unshuffled deck of cards
 
-	std::array<int, 52> current_deck;
-	int current_card;
+	std::array<int, 52> current_deck; ///< Current deck in use
+	int current_card; ///< Current card in the deck to reveal
 
-	std::random_device device;
-	std::default_random_engine generator;
+	std::random_device device; ///< Uniform random number device
+	std::default_random_engine generator; ///< RNG for shuffling
 };
